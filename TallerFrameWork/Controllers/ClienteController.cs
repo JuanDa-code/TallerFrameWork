e716c8d7b20a4fe7836a561ffdfe71a52ac42d04
JuanDa-code <juandax123456@gmail.com>
2021-08-10 15:26:40 -0500

    Crud Completo de Cliente y Rol (Index, Create, Delete, Details, Edit).

---
+++ TallerFrameWork/Controllers/ClienteController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TallerFrameWork.Models;
+
+namespace TallerFrameWork.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            using (var bd = new inventario2021Entities())
+            {
+                return View(bd.cliente.ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(cliente cliente)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var bd = new inventario2021Entities())
+                {
+                    bd.cliente.Add(cliente);
+                    bd.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            } catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (var bd = new inventario2021Entities())
+            {
+                var findCliente = bd.cliente.Find(id);
+                return View(findCliente);
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var bd = new inventario2021Entities())
+                {
+                    var findCliente = bd.cliente.Find(id);
+                    bd.cliente.Remove(findCliente);
+                    bd.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            } catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var bd = new inventario2021Entities())
+                {
+                    cliente findCliente = bd.cliente.Where(a => a.id == id).FirstOrDefault();
+                    return View(findCliente);
+                }
+            } catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit(cliente editCliente)
+        {
+            try
+            {
+                using (var bd = new inventario2021Entities())
+                {
+                    cliente cliente = bd.cliente.Find(editCliente.id);
+
+                    cliente.nombre = editCliente.nombre;
+                    cliente.email = editCliente.email;
+                    cliente.documento = editCliente.documento;
+
+                    bd.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            } catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+    }
+}
---
+++ TallerFrameWork/Controllers/RolController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TallerFrameWork.Models;
+
+namespace TallerFrameWork.Controllers
+{
+    public class RolController : Controller
+    {
+        // GET: Rol
+        public ActionResult Index()
+        {
+            using (var bd = new inventario2021Entities())
+            {
+                return View(bd.roles.ToList());
+            }
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(roles roles)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var bd = new inventario2021Entities())
+                {
+                    bd.roles.Add(roles);
+                    bd.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (var bd = new inventario2021Entities())
+            {
+                var findRoles = bd.roles.Find(id);
+                return View(findRoles);
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var bd = new inventario2021Entities())
+                {
+                    var findRoles = bd.roles.Find(id);
+                    bd.roles.Remove(findRoles);
+                    bd.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var bd = new inventario2021Entities())
+                {
+                    roles findRoles = bd.roles.Where(a => a.id == id).FirstOrDefault();
+                    return View(findRoles);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit(roles editRoles)
+        {
+            try
+            {
+                using (var bd = new inventario2021Entities())
+                {
+                    roles roles = bd.roles.Find(editRoles.id);
+
+                    roles.descripcion = editRoles.descripcion;
+
+                    bd.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+    }
+}
