22d71acbb49fb3964c438b7f45bbd0d162f4d1fa
JuanDa-code <juandax123456@gmail.com>
2021-08-11 11:40:07 -0500

    Creacion del Crud Producto con llave foranea

---
+++ TallerFrameWork/Controllers/ProductoController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TallerFrameWork.Models;
+
+namespace TallerFrameWork.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: Producto
+        public ActionResult Index()
+        {
+            using (var bd = new inventario2021Entities())
+            {
+                return View(bd.producto.ToList());
+            }
+        }
+
+        public static String NombreProveedor(int idProveedor)
+        {
+            using (var bd = new inventario2021Entities())
+            {
+                return bd.proveedor.Find(idProveedor).nombre;
+            }
+        }
+
+        public ActionResult ListarProveedores()
+        {
+            using (var bd = new inventario2021Entities())
+            {
+                return PartialView(bd.proveedor.ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(producto producto)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var bd = new inventario2021Entities())
+                {
+                    bd.producto.Add(producto);
+                    bd.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            } catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+    }
+}
