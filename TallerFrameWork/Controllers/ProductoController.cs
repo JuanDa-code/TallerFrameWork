ae99904ea9d8145a625b7724430b20879a3ada95
JuanDa-code <juandax123456@gmail.com>
2021-08-17 09:29:08 -0500

    Completando Crud de productos y modificacion del layout.

--- TallerFrameWork/Controllers/ProductoController.cs
+++ TallerFrameWork/Controllers/ProductoController.cs
@@ -61,5 +61,77 @@ namespace TallerFrameWork.Controllers
                 return View();
             }
         }
+
+        public ActionResult Details (int id)
+        {
+            using ( var bd = new inventario2021Entities())
+            {
+                return View(bd.producto.Find(id));
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var bd = new inventario2021Entities())
+                {
+                    producto findProducto = bd.producto.Where(a => a.id == id).FirstOrDefault();
+                    return View(findProducto);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit(producto editProducto)
+        {
+            try
+            {
+                using (var bd = new inventario2021Entities())
+                {
+                    producto producto = bd.producto.Find(editProducto.id);
+
+                    producto.nombre = editProducto.nombre;
+                    producto.cantidad = editProducto.cantidad;
+                    producto.descripcion = editProducto.descripcion;
+                    producto.percio_unitario = editProducto.percio_unitario;
+                    producto.proveedor = editProducto.proveedor;
+
+                    bd.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var bd = new inventario2021Entities())
+                {
+                    producto producto = bd.producto.Find(id);
+                    bd.producto.Remove(producto);
+                    bd.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
     }
 }
