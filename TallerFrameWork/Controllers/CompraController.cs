c85ca3828c7197790349774f020dc87b4956ecb4
JuanDa-code <juandax123456@gmail.com>
2021-08-18 18:11:26 -0500

    Creacion del crud de Compra, y se agrego al layout opcion de compra.

---
+++ TallerFrameWork/Controllers/CompraController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TallerFrameWork.Models;
+
+namespace TallerFrameWork.Controllers
+{
+    public class CompraController : Controller
+    {
+        // GET: Compra
+        public ActionResult Index()
+        {
+            using (var bd = new inventario2021Entities())
+            {
+                return View(bd.compra.ToList());
+            }
+        }
+
+        public static String NombreUsuario (int idUsuario)
+        {
+            using (var bd = new inventario2021Entities())
+            {
+                return bd.usuario.Find(idUsuario).nombre;
+            }
+        }
+
+        public static String NombreCliente (int idCliente)
+        {
+            using (var bd = new inventario2021Entities())
+            {
+                return bd.cliente.Find(idCliente).nombre;
+            }
+        }
+
+        public ActionResult ListarUsuarios ()
+        {
+            using (var bd = new inventario2021Entities())
+            {
+                return PartialView(bd.usuario.ToList());
+            }
+        }
+
+        public ActionResult ListarClientes ()
+        {
+            using (var bd = new inventario2021Entities())
+            {
+                return PartialView(bd.cliente.ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(compra compra)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var bd = new inventario2021Entities())
+                {
+                    bd.compra.Add(compra);
+                    bd.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Details (int id)
+        {
+            using ( var bd = new inventario2021Entities())
+            {
+                return View(bd.compra.Find(id));
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var bd = new inventario2021Entities())
+                {
+                    compra findCompra = bd.compra.Where(a => a.id == id).FirstOrDefault();
+                    return View(findCompra);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit(compra editCompra)
+        {
+            try
+            {
+                using (var bd = new inventario2021Entities())
+                {
+                    compra compra = bd.compra.Find(editCompra.id);
+
+                    compra.fecha = editCompra.fecha;
+                    compra.total = editCompra.total;
+                    compra.id_usuario = editCompra.id_usuario;
+                    compra.id_cliente = editCompra.id_cliente;
+
+                    bd.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Delete (int id)
+        {
+            try
+            {
+                using (var bd = new inventario2021Entities())
+                {
+                    compra compra = bd.compra.Find(id);
+                    bd.compra.Remove(compra);
+                    bd.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+    }
+}
