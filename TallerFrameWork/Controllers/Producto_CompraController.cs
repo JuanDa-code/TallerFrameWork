0f35ed8c517127bc7d908ca7e1c87df78f83b5bb
JuanDa-code <juandax123456@gmail.com>
2021-08-19 13:16:44 -0500

    Creacion y terminacion de cruds Producto imagen y producto compra, terminacion de todas las tablas.

---
+++ TallerFrameWork/Controllers/Producto_CompraController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TallerFrameWork.Models;
+
+namespace TallerFrameWork.Controllers
+{
+    public class Producto_CompraController : Controller
+    {
+        // GET: Producto_Compra
+        public ActionResult Index()
+        {
+            using (var bd = new inventario2021Entities())
+            {
+                return View(bd.producto_compra.ToList());
+            }
+        }
+
+        public static String NombreProducto (int idProducto)
+        {
+            using (var bd = new inventario2021Entities())
+            {
+                return bd.producto.Find(idProducto).nombre;
+            }
+        }
+
+        public ActionResult ListarProductos()
+        {
+            using (var bd = new inventario2021Entities())
+            {
+                return PartialView(bd.producto.ToList());
+            }
+        }
+
+        public static DateTime FechaCompra(int idCompra)
+        {
+            using (var bd = new inventario2021Entities())
+            {
+                return bd.compra.Find(idCompra).fecha;
+            }
+        }
+
+        public ActionResult ListarCompras()
+        {
+            using (var bd = new inventario2021Entities())
+            {
+                return PartialView(bd.compra.ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(producto_compra producto_compra)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var bd = new inventario2021Entities())
+                {
+                    bd.producto_compra.Add(producto_compra);
+                    bd.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            } catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Details (int id)
+        {
+            using (var bd = new inventario2021Entities())
+            {
+                return View(bd.producto_compra.Find(id));
+            }
+        }
+
+        public ActionResult Edit (int id)
+        {
+            try
+            {
+                using (var bd = new inventario2021Entities())
+                {
+                    producto_compra findProductoCompra = bd.producto_compra.Where(a => a.id == id).FirstOrDefault();
+                    return View(findProductoCompra);
+                }
+            } catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit (producto_compra editProductoCompra)
+        {
+            try
+            {
+                using (var bd = new inventario2021Entities())
+                {
+                    producto_compra producto_compra = bd.producto_compra.Find(editProductoCompra.id);
+
+                    producto_compra.id_compra = editProductoCompra.id_compra;
+                    producto_compra.id_producto = editProductoCompra.id_producto;
+                    producto_compra.cantidad = editProductoCompra.cantidad;
+
+                    bd.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            } catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var bd = new inventario2021Entities())
+                {
+                    producto_compra producto_compra = bd.producto_compra.Find(id);
+                    bd.producto_compra.Remove(producto_compra);
+                    bd.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            } catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+    }
+}
---
+++ TallerFrameWork/Controllers/Producto_ImagenController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TallerFrameWork.Models;
+
+namespace TallerFrameWork.Controllers
+{
+    public class Producto_ImagenController : Controller
+    {
+        // GET: Producto_Imagen
+        public ActionResult Index()
+        {
+            using (var bd = new inventario2021Entities())
+            {
+                return View(bd.producto_imagen.ToList());
+            }
+        }
+
+        public static String NombreProducto (int idProducto)
+        {
+            using (var bd = new inventario2021Entities())
+            {
+                return bd.producto.Find(idProducto).nombre;
+            }
+        }
+
+        public ActionResult ListarProductos()
+        {
+            using (var bd = new inventario2021Entities())
+            {
+                return PartialView(bd.producto.ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(producto_imagen producto_Imagen)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var bd = new inventario2021Entities())
+                {
+                    bd.producto_imagen.Add(producto_Imagen);
+                    bd.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            } catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Details (int id)
+        {
+            using (var bd = new inventario2021Entities())
+            {
+                return View(bd.producto_imagen.Find(id));
+            }
+        }
+
+        public ActionResult Edit (int id)
+        {
+            try
+            {
+                using (var bd = new inventario2021Entities())
+                {
+                    producto_imagen findProductoImagen = bd.producto_imagen.Where(a => a.id == id).FirstOrDefault();
+                    return View(findProductoImagen);
+                }
+            } catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit (producto_imagen editProductoImagen)
+        {
+            try
+            {
+                using (var bd = new inventario2021Entities())
+                {
+                    producto_imagen producto_imagen = bd.producto_imagen.Find(editProductoImagen.id);
+
+                    producto_imagen.imagen = editProductoImagen.imagen;
+                    producto_imagen.id_producto = editProductoImagen.id_producto;
+
+                    bd.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Delete (int id)
+        {
+            try
+            {
+                using (var bd = new inventario2021Entities())
+                {
+                    producto_imagen producto_imagen = bd.producto_imagen.Find(id);
+                    bd.producto_imagen.Remove(producto_imagen);
+                    bd.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            } catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+    }
+}
