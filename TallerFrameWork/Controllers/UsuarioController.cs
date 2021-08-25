37c3b8c0d78e4ae9c37d98e45fac7ab00783ef70
JuanDa-code <juandax123456@gmail.com>
2021-08-25 10:33:14 -0500

    Creacion del login para usuario

--- TallerFrameWork/Controllers/UsuarioController.cs
+++ TallerFrameWork/Controllers/UsuarioController.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TallerFrameWork.Models;
+using System.Web.Security;
+using System.Text;
 
 namespace TallerFrameWork.Controllers
 {
     public class UsuarioController : Controller
     {
+        [Authorize]
         // GET: Usuario
         public ActionResult Index()
         {
@@ -35,6 +38,7 @@ namespace TallerFrameWork.Controllers
             {
                 using (var bd = new inventario2021Entities())
                 {
+                    usuario.password = UsuarioController.HashSHA1(usuario.password);
                     bd.usuario.Add(usuario);
                     bd.SaveChanges();
                     return RedirectToAction("Index");
@@ -45,6 +49,20 @@ namespace TallerFrameWork.Controllers
                 ModelState.AddModelError("", "Error" + ex);
                 return View();
             }
+        }
+
+        public static string HashSHA1(string value)
+        {
+            var sha1 = System.Security.Cryptography.SHA1.Create();
+            var inputBytes = Encoding.ASCII.GetBytes(value);
+            var hash = sha1.ComputeHash(inputBytes);
+
+            var sb = new StringBuilder();
+            for ( var i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
         }
 
         public ActionResult Details(int id)
@@ -118,6 +136,48 @@ namespace TallerFrameWork.Controllers
                 ModelState.AddModelError("", "Error " + ex);
                 return View();
             }
+        }
+
+        public ActionResult Login(string mensaje = "")
+        {
+            ViewBag.Message = mensaje;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string user, string password)
+        {
+            try
+            {
+                string passEncrip = UsuarioController.HashSHA1(password);
+                using (var db = new inventario2021Entities())
+                {
+                    var userLogin = db.usuario.FirstOrDefault(e => e.email == user && e.password == passEncrip);
+                    if (userLogin != null)
+                    {
+                        FormsAuthentication.SetAuthCookie(userLogin.email, true);
+                        Session["User"] = userLogin;
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        return Login("Verifique sus datos");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        [Authorize]
+        public ActionResult CloseSession()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
